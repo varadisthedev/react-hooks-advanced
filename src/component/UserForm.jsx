5eb33474a46211c6cb8d10c04fa50120de24cfd2
varadisthedev <varadisthedev@gmail.com>
2025-10-06 03:38:49 +0530

    update userform css using tailwind

--- src/component/UserForm.jsx
+++ src/component/UserForm.jsx
@@ -1,5 +1,7 @@
 import React,{useState} from 'react'
 import useInput from '../hooks/useInput'
+import user_blue from '../assets/user_blu.png'
+import user from '../assets/user.png'
 
 function UserForm() {
 
@@ -13,17 +15,21 @@ function UserForm() {
         resetLastName()
     }
   return (
-    <div>
-        <form onSubmit={submitHandler}>
+    <div >
+        <form className='flex flex-col items-center justify-center w-110 h-150 bg-gradient-to-br from-blue-200 to-blue-400 ' onSubmit={submitHandler}>
+            <img src={user_blue} className='w-30 h-30 mt-15'></img>
+            <h1 className='font-extrabold text-5xl text-blue-'>Login portal</h1>
+
             <div>
-                <label>First name:</label>
-                <input {...bindFirstName} type='text'></input>
+                {/* <label >first name</label> */}
+                <input {...bindFirstName}  className='text-xl mt-7 font-medium px-3 py-1 rounded-full bg-white/10 shadow-md  border-1 w-100' placeholder='First name' type='text'></input>
+                <img></img>
             </div>
             <div>
-                <label>Last name:</label>
-                <input type='text' {...bindLastName}></input>
+                <input {...bindLastName}  className='m-3 text-xl mt-5 font-medium px-3 py-1 rounded-full bg-white/10 shadow-md  border-1 w-100' placeholder='Last name' type='password'></input>
+                <img></img>
             </div>
-            <button>submit</button>
+            <button className='text-3xl font-medium text-black bg-white rounded-3xl px-3 py-1 m-3 shadow-md border-black w-100' >login</button>
         </form>
     </div>
   )
